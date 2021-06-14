add some plots, remove softmax, and other minor fixes

factory_env.py
@@ -14,9 +14,7 @@ class FactoryEnv:
 
     def step(self, action):
         if action == -1:
-            self.take_time_step()
-            # We use the same API as gym environments : https://gym.openai.com/docs/
-            return self.get_state(), -1, self.check_done(), None
+            pass  # This action correspond to noop
         elif action in range(0, self.n_jobs):
             if np.sum(self.current_jobs[action]) == 0:
                 indexes = np.where(self.completion[action] == 0)[0]
@@ -24,12 +22,17 @@ class FactoryEnv:
                     index = indexes[0]
                     if self.machine_usage[self.affectations[action][index]] == 0:
                         self.current_jobs[action][index] = 1
-            self.take_time_step()
-            return self.get_state(), -1, self.check_done(), None
         else: 
             raise Exception(f"The action you provided ({action}) is not valid. "
             f"Please provide an integer between -1 and {self.n_jobs}.")
-    
+        
+        self.take_time_step()
+        reward = -1
+        if self.check_done():
+            reward = 10
+        # We use the same API as gym environments : https://gym.openai.com/docs/
+        return self.get_state(), reward, self.check_done(), None
+         
     def render(self, verbosity=0):
         print("PROBLEM DESCRIPTION")
         print("Affectations of jobs on machines")
@@ -65,7 +68,7 @@ class FactoryEnv:
         #  - a second one representing the times needed for each job to complete
         #  - and a third one saying how long the job has been running.
         # The third matrix is the only one which is going to change. Once the third matrix
-        # Is similar to the second one, all jobs are completed.
+        # is similar to the second one, all jobs are completed.
         return np.concatenate((self.affectations, self.times, self.completion), axis=0)
 
     def check_done(self):

main.py
@@ -10,35 +10,50 @@ from benchmark import AFFECTATIONS, TIMES
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-n_jobs = 15 
-n_machines = 15
+n_jobs = 2 
+n_machines = 2
 
-# affectations = np.floor(np.random.uniform(0, n_machines, (n_jobs, n_machines))).astype(np.int32) 
-# times = np.floor(np.random.uniform(1, 10, (n_jobs, n_machines))).astype(np.int32) 
-affectations = np.array(AFFECTATIONS) - 1
-times = np.array(TIMES)
+affectations = np.floor(np.random.uniform(0, n_machines, (n_jobs, n_machines))).astype(np.int32) 
+times = np.floor(np.random.uniform(1, 10, (n_jobs, n_machines))).astype(np.int32) 
+
+print(affectations)
+print(times)
+time.sleep(2)
+
+# affectations = np.array(AFFECTATIONS) - 1
+# times = np.array(TIMES)
+
+hidden_size = 8
 
 env = FactoryEnv(n_jobs, n_machines, affectations, times) 
 agent = SimpleAgent(n_jobs, 
                     n_machines, 
                     env.get_state_space_dimension(), 
-                    env.get_action_space_dimension(), 
+                    env.get_action_space_dimension(),
+                    hidden_size,
                     device)
 
-n_episodes = 1000 
+n_episodes = 5000 
 sum_steps = 0
+
+min_achieved_boundary = 2 * np.sum(times)  # Take a high value, just in case
 
 for i in range(n_episodes):
     env.reset()
     state = env.get_state()
     done = False
     n_steps = 0
+    actions_taken = []
+    values = []
 
     while not done:
-        action = int(np.floor(np.random.uniform(-1, n_jobs)))  # Random agent
+        # action = int(np.floor(np.random.uniform(-1, n_jobs)))  # Random agent
     
         # Take an action w.r.t the agent policy
-        # action = agent.select_action(state)
+        action = agent.select_action(state)
+
+        # Compute the value of the taken action
+        value = agent.policy_net(agent.convert_state_to_net_input(state))[action].item()
 
         # Get the resulting reward and next state from environment
         next_state, reward, done, _ = env.step(action)
@@ -47,26 +62,32 @@ for i in range(n_episodes):
             next_state = None
 
         # Store the (s, a, r, sp) quadruplet for training
-        # agent.store(state, action, reward, next_state)
+        agent.store(state, action, reward, next_state)
 
         # Set next_state as the new state
         state = next_state
 
         # Make one agent training step
-        # agent.train_one_step()
+        agent.train_one_step()
 
         #if n_steps % 200 == 0:
         #    env.render(verbosity=0)
         
         n_steps += 1
+        actions_taken.append(action)
+        values.append(value)
 
     sum_steps += n_steps
+    min_achieved_boundary = min(min_achieved_boundary, n_steps)
     print(f"Job done in {n_steps * env.time_step} units of time")
     print(f"Average time : {sum_steps / (i + 1)} steps")
+    print(actions_taken)
+    print(values)
 
 #env.render()
 print(f"Job done in {n_steps * env.time_step} units of time")
 print(f"Minimum boudary for time : {np.max(np.sum(times, axis=1))}")
+print(f"Minimum achieved boudary for time : {min_achieved_boundary}")
 print(f"Maximum boudary for time : {np.sum(times)}")
 
 pickle.dump(agent, open("./simple_agent_save.pickle", "wb"))

simple_agent.py
@@ -10,37 +10,41 @@ from memory import ReplayMemory, Transition
 
 class SimpleAgent:
 
-    def __init__(self, n_jobs, n_machines, input_size, n_actions, device):
+    def __init__(self, n_jobs, n_machines, input_size, n_actions, hidden_size, device):
         self.batch_size = 512
         self.gamma = 0.999
         self.eps_start = 0.9
-        self.eps_end = 0.05
-        self.eps_decay= 200
-        self.target_update = 10
+        self.eps_end = 0.
+        self.eps_decay = 5000
+        self.target_update = 1000
 
         self.n_actions = n_actions
         self.device = device
 
-        self.policy_net = DQN(input_size, n_actions, device)
-        self.target_net = DQN(input_size, n_actions, device)
+        self.policy_net = DQN(input_size, n_actions, hidden_size, device)
+        self.target_net = DQN(input_size, n_actions, hidden_size, device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
         
-        self.memory = ReplayMemory(1000000)
+        self.memory = ReplayMemory(10000)
 
         self.steps_done = 0
 
         self.is_training = True
-        self. optimizer = optim.RMSprop(self.policy_net.parameters())
-    
+        self.optimizer = optim.RMSprop(self.policy_net.parameters())
+   
+    @classmethod
+    def convert_state_to_net_input(cls, state):
+        return torch.tensor(state).view(-1).float() # Reshape tensor to make it a column
+
     def select_action(self, state):
         sample = random.random()
         eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
             math.exp(-1. * self.steps_done / self.eps_decay)
         if sample > eps_threshold or not self.is_training:
             with torch.no_grad():
-                state = torch.tensor(state).view(-1).float()  # Reshape tensor to make it a column
-                return self.policy_net(state).argmax().item()  # item is here to convert tensor to int
+                net_input = self.convert_state_to_net_input(state)
+                return self.policy_net(net_input).argmax().item()  # item is here to convert tensor to int
         else:
             return random.randrange(self.n_actions)
 
@@ -84,7 +88,11 @@ class SimpleAgent:
         for param in self.policy_net.parameters():
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
- 
+
+        self.steps_done += 1
+        if self.steps_done % self.target_update == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+
     def train(self):
         self.is_training = True
 

simple_dqn_model.py
@@ -4,14 +4,13 @@ import torch.nn as nn
 
 class DQN(nn.Module):
 
-    def __init__(self, input_size, output_size, device):
+    def __init__(self, input_size, output_size, hidden_size, device):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 2048)
+        self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(2048, 2048) 
+        self.fc2 = nn.Linear(hidden_size, hidden_size) 
         self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(2048, output_size) 
-        self.softmax = nn.Softmax(dim=0)
+        self.fc3 = nn.Linear(hidden_size, output_size) 
         self.to(device)
         self.device = device
 
@@ -19,6 +18,6 @@ class DQN(nn.Module):
         x = x.to(self.device)
         x = self.relu1(self.fc1(x))
         x = self.relu2(self.fc2(x))
-        x = self.softmax(self.fc3(x))
+        x = self.fc3(x)
         return x
 