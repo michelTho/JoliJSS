4fca3e138b4e8e01f18f314dc786812495f23bc4
Michel Thomazo <thomazo.michel@gmail.com>
2021-06-29 16:16:13 +0200

    remove useless import

--- dqn_vs_vi.py
+++ dqn_vs_vi.py
@@ -3,7 +3,6 @@ from copy import copy
 from datetime import datetime
 import time                                                                             
             
-import gnuplotlib as gp
 import matplotlib.pyplot as plt
 import numpy as np                                                                      
 import torch   
